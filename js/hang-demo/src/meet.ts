4cc0cae243ac01cedea536ed191f285ef1673852
Luke Curley <kixelated@gmail.com>
2025-06-23 11:44:30 -0700

    And revamp the demos a bit.

---
+++ js/hang-demo/src/meet.ts
@@ -0,0 +1,7 @@
+import "./highlight";
+
+import HangSupport from "@kixelated/hang/support/element";
+import HangMeet from "@kixelated/hang/meet/element";
+import HangPublish from "@kixelated/hang/publish/element";
+
+export { HangMeet, HangSupport, HangPublish };
