747c0ead82f6fcad446097e4ab6e1e896fd75df9
gordysunsaronic <gordy.sun@saronic.com>
2025-06-30 11:07:09 -0500

    chore: i hate this

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    # js.url = "./js";  # Commented out to fix PSM build issues
+    js.url = "./js";
     rs.url = "./rs";
   };
 
@@ -14,7 +14,7 @@
       self,
       nixpkgs,
       flake-utils,
-      # js,  # Commented out
+      js,
       rs,
       ...
     }:
@@ -26,7 +26,7 @@
       devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
         inputsFrom = [
           rs.devShells.${system}.default
-          # js.devShells.${system}.default  # Commented out
+          js.devShells.${system}.default 
         ];
         shellHook = ''
           echo "🎯 Using Gordy's Top-Level MoQ Flake (Rust environment only)"
