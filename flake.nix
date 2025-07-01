879a67969a90aa246be71a50d9e79a2e3ad41e17
gordysunsaronic <gordy.sun@saronic.com>
2025-07-01 16:21:15 -0500

    chore: breaking the flake.nix files again

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
-  description = "MoQ - Media over QUIC";
+  description = "Top-level flake delegating to rs and js";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix.url = "github:nix-community/fenix";
-    naersk.url = "github:nmattia/naersk";
-    
-    # Keep js as a separate flake
+
     js.url = "./js";
+    rs.url = "./rs";
   };
 
   outputs =
@@ -16,110 +14,24 @@
       self,
       nixpkgs,
       flake-utils,
-      fenix,
-      naersk,
       js,
+      rs,
+      ...
     }:
     {
-      nixosModules = {
-        moq-relay = import ./rs/nix/modules/moq-relay.nix;
-      };
-      
-      overlays.default = import ./rs/nix/overlay.nix;
+      nixosModules = rs.nixosModules;
+      overlays = rs.overlays;
     }
-    // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
-        # Rust toolchain setup
-        rust =
-          with fenix.packages.${system};
-          combine [
-            stable.rustc
-            stable.cargo
-            stable.clippy
-            stable.rustfmt
-            targets.wasm32-unknown-unknown.stable.rust-std
-          ];
-
-        naersk' = naersk.lib.${system}.override {
-          cargo = rust;
-          rustc = rust;
-        };
-
-        # GStreamer dependencies
-        gst-deps = with pkgs.gst_all_1; [
-          gstreamer
-          gst-plugins-base
-          gst-plugins-good
-          gst-plugins-bad
-          gst-plugins-ugly
-          gst-libav
+    // flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
+        inputsFrom = [
+          rs.devShells.${system}.default
+          js.devShells.${system}.default
         ];
-
-        # Common Rust dependencies
-        rust-deps = [
-          rust
-          pkgs.just
-          pkgs.pkg-config
-          pkgs.glib
-          pkgs.libressl
-          pkgs.ffmpeg
-        ] ++ gst-deps;
-
-      in
-      {
-        packages = {
-          # Rust packages
-          moq-clock = naersk'.buildPackage {
-            pname = "moq-clock";
-            src = ./rs;
-          };
-
-          moq-relay = naersk'.buildPackage {
-            pname = "moq-relay";
-            src = ./rs;
-          };
-
-          hang = naersk'.buildPackage {
-            pname = "hang";
-            src = ./rs;
-          };
-
-          default = self.packages.${system}.moq-relay;
-        };
-
-        devShells = {
-          # Combined Rust + JS development shell
-          default = pkgs.mkShell {
-            inputsFrom = [ js.devShells.${system}.default ];
-            packages = rust-deps ++ [
-              pkgs.cargo-sort
-              pkgs.cargo-shear
-              pkgs.cargo-audit
-            ];
-            
-            shellHook = ''
-              export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
-            '';
-          };
-
-          # Rust-only development shell
-          rust = pkgs.mkShell {
-            packages = rust-deps ++ [
-              pkgs.cargo-sort
-              pkgs.cargo-shear
-              pkgs.cargo-audit
-            ];
-            
-            shellHook = ''
-              export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
-            '';
-          };
-        };
-      }
-    );
+        shellHook = "";
+      };
+      packages = {
+        inherit (rs.packages.${system}) moq-relay moq-clock hang;
+      };
+    });
 }
---
+++ rs/flake.nix
@@ -0,0 +1,103 @@
+{
+  description = "MoQ";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+    fenix.url        = "github:nix-community/fenix";
+    naersk.url       = "github:nmattia/naersk";
+  };
+
+  outputs =
+    {
+      self,
+      fenix,
+      nixpkgs,
+      flake-utils,
+      naersk,
+    }:
+    {
+      nixosModules = {
+        moq-relay = import ./nix/modules/moq-relay.nix;
+      };
+      
+      overlays.default = import ./nix/overlay.nix;
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        rust =
+          with fenix.packages.${system};
+          combine [
+            stable.rustc
+            stable.cargo
+            stable.clippy
+            stable.rustfmt
+            targets.wasm32-unknown-unknown.stable.rust-std
+          ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = rust;
+          rustc = rust;
+        };
+
+        gst-deps = with pkgs.gst_all_1; [
+          gstreamer
+          gst-plugins-base
+          gst-plugins-good
+          gst-plugins-bad
+          gst-plugins-ugly
+          gst-libav
+        ];
+
+        common-deps = [
+          rust
+          pkgs.just
+          pkgs.pkg-config
+          pkgs.glib
+          pkgs.libressl
+          pkgs.ffmpeg
+        ] ++ gst-deps;
+
+      in
+      {
+        packages = {
+          moq-clock = naersk'.buildPackage {
+            pname = "moq-clock";
+            src = ./.;
+          };
+
+          moq-relay = naersk'.buildPackage {
+            pname = "moq-relay";
+            src = ./.;
+          };
+
+          hang = naersk'.buildPackage {
+            pname = "hang";
+            src = ./.;
+          };
+
+          default = naersk'.buildPackage {
+            src = ./.;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = common-deps ++ [
+            pkgs.cargo-sort
+            pkgs.cargo-shear
+            pkgs.cargo-audit
+          ];
+          
+          # Environment variables from moq-rs
+          shellHook = ''
+            export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
+          '';
+        };
+      }
+    );
+}
