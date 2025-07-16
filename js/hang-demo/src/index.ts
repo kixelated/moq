a564a250f18c1e9f682c26945173ccdccffeedb0
Luke Curley <kixelated@gmail.com>
2025-07-16 07:27:40 -0400

    Revert the hang-demo changes.

--- js/hang-demo/src/index.ts
+++ js/hang-demo/src/index.ts
@@ -7,8 +7,7 @@ export { HangWatch, HangSupport };
 
 const watch = document.querySelector("hang-watch") as HangWatch;
 
-// If query params are provided, use it as the broadcast name
+// If query params are provided, use it as the broadcast name.
 const urlParams = new URLSearchParams(window.location.search);
-const name = urlParams.get("name") ?? "jwt-test/bbb";
-watch.setAttribute("url", `http://localhost:4443/demo/`);
-watch.setAttribute("path", ``);
+const name = urlParams.get("name") ?? "demo/bbb";
+watch.setAttribute("url", `http://localhost:4443/${name}.hang`);
