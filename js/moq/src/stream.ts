ae2904f3bdc0b2681cecbaa927ac9939cb681cfc
Luke Curley <kixelated@gmail.com>
2025-07-25 16:20:10 -0700

    fmt

--- js/moq/src/stream.ts
+++ js/moq/src/stream.ts
@@ -101,7 +101,11 @@ export class Reader {
 	// Consumes the first size bytes of the buffer.
 	#slice(size: number): Uint8Array {
 		const result = new Uint8Array(this.#buffer.buffer, this.#buffer.byteOffset, size);
-		this.#buffer = new Uint8Array(this.#buffer.buffer, this.#buffer.byteOffset + size, this.#buffer.byteLength - size);
+		this.#buffer = new Uint8Array(
+			this.#buffer.buffer,
+			this.#buffer.byteOffset + size,
+			this.#buffer.byteLength - size,
+		);
 
 		return result;
 	}
