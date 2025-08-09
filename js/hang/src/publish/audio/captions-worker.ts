07312cb0fed139504404b0b784feddba73efa31d
Luke Curley <kixelated@gmail.com>
2025-08-08 21:34:05 -0700

    Add a back buffer to VAD.

--- js/hang/src/publish/audio/captions-worker.ts
+++ js/hang/src/publish/audio/captions-worker.ts
@@ -36,18 +36,25 @@ export interface Error {
 const SAMPLE_RATE = 16000;
 
 const VAD_CHUNK_SIZE = 512; // This VAD model expects 512 samples at a time, or 31ms
+const VAD_SILENCE_PADDING = 4;
 
 class Vad {
 	whisper: Whisper;
 
-	#queued = new Float32Array(new ArrayBuffer(Float32Array.BYTES_PER_ELEMENT * VAD_CHUNK_SIZE), 0, 0);
-	#swap = new ArrayBuffer(Float32Array.BYTES_PER_ELEMENT * VAD_CHUNK_SIZE);
+	// Simple circular buffer, primarily so we keep the previous buffer around once speaking is detected.
+	#next = new Float32Array(new ArrayBuffer(Float32Array.BYTES_PER_ELEMENT * VAD_CHUNK_SIZE), 0, 0); // queued
+	#current = new Float32Array(new ArrayBuffer(Float32Array.BYTES_PER_ELEMENT * VAD_CHUNK_SIZE), 0, 0); // being processed
+	#prev = new Float32Array(new ArrayBuffer(Float32Array.BYTES_PER_ELEMENT * VAD_CHUNK_SIZE), 0, 0); // already processed
+
 	#processing = false;
 
 	// Initial state for VAD
 	#sr = new Tensor("int64", [SAMPLE_RATE], []);
 	#state = new Tensor("float32", new Float32Array(2 * 1 * 128), [2, 1, 128]);
 	#speaking = false;
+
+	// Count the number of silence results, if we get 3 in a row then we're done.
+	#silence = 0;
 
 	#model: Promise<PreTrainedModel>;
 
@@ -62,17 +69,17 @@ class Vad {
 	}
 
 	write(samples: Float32Array) {
-		if (this.#queued.byteLength >= this.#queued.buffer.byteLength) {
+		if (this.#next.byteLength >= this.#next.buffer.byteLength) {
 			if (!this.flush()) {
 				// Drop the sample if VAD is still processing.
 				return;
 			}
 		}
 
-		this.#queued = new Float32Array(this.#queued.buffer, 0, this.#queued.length + samples.length);
-		this.#queued.set(samples, this.#queued.length - samples.length);
+		this.#next = new Float32Array(this.#next.buffer, 0, this.#next.length + samples.length);
+		this.#next.set(samples, this.#next.length - samples.length);
 
-		if (this.#queued.byteLength === this.#queued.buffer.byteLength) {
+		if (this.#next.byteLength === this.#next.buffer.byteLength) {
 			this.flush(); // don't care if it fails
 		}
 	}
@@ -84,21 +91,21 @@ class Vad {
 
 		this.#processing = true;
 
-		const queued = this.#queued;
-		this.#queued = new Float32Array(this.#swap, 0, 0);
-		this.#swap = queued.buffer;
+		this.#current = this.#next;
+		this.#next = new Float32Array(this.#prev.buffer, 0, 0);
+		this.#prev = this.#current;
 
-		this.#flush(queued).finally(() => {
+		this.#flush().finally(() => {
 			this.#processing = false;
 		});
 
 		return true;
 	}
 
-	async #flush(buffer: Float32Array) {
+	async #flush() {
 		const model = await this.#model;
 
-		const input = new Tensor("float32", buffer, [1, buffer.length]);
+		const input = new Tensor("float32", this.#current, [1, this.#current.length]);
 		const result = await model({ input, sr: this.#sr, state: this.#state });
 		this.#state = result.stateN;
 
@@ -106,14 +113,19 @@ class Vad {
 
 		const isSpeech = result.output.data[0];
 		if (this.#speaking && isSpeech < 0.3) {
-			this.#speaking = false;
+			this.#silence++;
 
-			postResult({
-				type: "speaking",
-				speaking: false,
-			});
+			if (this.#silence > VAD_SILENCE_PADDING) {
+				this.#speaking = false;
+
+				postResult({
+					type: "speaking",
+					speaking: false,
+				});
+			}
 		} else if (!this.#speaking && isSpeech >= 0.1) {
 			this.#speaking = true;
+			this.#silence = 0;
 
 			postResult({
 				type: "speaking",
@@ -121,8 +133,12 @@ class Vad {
 			});
 		}
 
+		if (!wasSpeaking && this.#speaking) {
+			this.whisper.write(this.#prev);
+		}
+
 		if (wasSpeaking || this.#speaking) {
-			this.whisper.write(buffer);
+			this.whisper.write(this.#current);
 		}
 
 		if (wasSpeaking && !this.#speaking) {
