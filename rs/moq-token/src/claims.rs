1483517ba771214e13bc21cbaf8bb816fcd05691
Luke Curley <kixelated@gmail.com>
2025-08-12 10:56:38 -0700

    Add support for either a string or a vec.

--- rs/moq-token/src/claims.rs
+++ rs/moq-token/src/claims.rs
@@ -1,8 +1,25 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 use serde_with::{serde_as, TimestampSeconds};
 
 fn is_false(value: &bool) -> bool {
 	!value
+}
+
+fn string_or_vec<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+where
+	D: Deserializer<'de>,
+{
+	#[derive(Deserialize)]
+	#[serde(untagged)]
+	enum StringOrVec {
+		String(String),
+		Vec(Vec<String>),
+	}
+
+	match StringOrVec::deserialize(deserializer)? {
+		StringOrVec::String(s) => Ok(vec![s]),
+		StringOrVec::Vec(v) => Ok(v),
+	}
 }
 
 #[serde_as]
@@ -17,7 +34,7 @@ pub struct Claims {
 
 	/// If specified, the user can publish any matching broadcasts.
 	/// If not specified, the user will not publish any broadcasts.
-	#[serde(rename = "put", skip_serializing_if = "Vec::is_empty")]
+	#[serde(default, rename = "put", skip_serializing_if = "Vec::is_empty", deserialize_with = "string_or_vec")]
 	pub publish: Vec<String>,
 
 	/// If true, then this client is considered a cluster node.
@@ -31,7 +48,7 @@ pub struct Claims {
 	/// If specified, the user can subscribe to any matching broadcasts.
 	/// If not specified, the user will not receive announcements and cannot subscribe to any broadcasts.
 	// NOTE: This can't be renamed to "sub" because that's a reserved JWT field.
-	#[serde(rename = "get", skip_serializing_if = "Vec::is_empty")]
+	#[serde(default, rename = "get", skip_serializing_if = "Vec::is_empty", deserialize_with = "string_or_vec")]
 	pub subscribe: Vec<String>,
 
 	/// The expiration time of the token as a unix timestamp.
@@ -266,5 +283,44 @@ mod tests {
 	fn test_is_false_helper() {
 		assert!(is_false(&false));
 		assert!(!is_false(&true));
+	}
+
+	#[test]
+	fn test_deserialize_string_as_vec() {
+		let json = r#"{
+			"root": "test",
+			"put": "single-publish",
+			"get": "single-subscribe"
+		}"#;
+
+		let claims: Claims = serde_json::from_str(json).unwrap();
+		assert_eq!(claims.publish, vec!["single-publish"]);
+		assert_eq!(claims.subscribe, vec!["single-subscribe"]);
+	}
+
+	#[test]
+	fn test_deserialize_vec_as_vec() {
+		let json = r#"{
+			"root": "test",
+			"put": ["pub1", "pub2"],
+			"get": ["sub1", "sub2"]
+		}"#;
+
+		let claims: Claims = serde_json::from_str(json).unwrap();
+		assert_eq!(claims.publish, vec!["pub1", "pub2"]);
+		assert_eq!(claims.subscribe, vec!["sub1", "sub2"]);
+	}
+
+	#[test]
+	fn test_deserialize_mixed() {
+		let json = r#"{
+			"root": "test",
+			"put": "single",
+			"get": ["multi1", "multi2"]
+		}"#;
+
+		let claims: Claims = serde_json::from_str(json).unwrap();
+		assert_eq!(claims.publish, vec!["single"]);
+		assert_eq!(claims.subscribe, vec!["multi1", "multi2"]);
 	}
 }
