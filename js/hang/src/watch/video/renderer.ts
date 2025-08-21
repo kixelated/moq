b9eecca8ac673d2a14761d7a6b6185866baf5974
Luke Curley <kixelated@gmail.com>
2025-08-21 16:36:54 -0700

    Use new name.

--- js/hang/src/watch/video/renderer.ts
+++ js/hang/src/watch/video/renderer.ts
@@ -43,7 +43,7 @@ export class VideoRenderer {
 			if (info) {
 				// Initialize the canvas to the correct size.
 				// NOTE: each frame will resize the canvas, so this is mostly to avoid pop-in.
-				canvas.width = info.config.displayAspectWidth?? info.config.codedWidth ?? 1;
+				canvas.width = info.config.displayAspectWidth ?? info.config.codedWidth ?? 1;
 				canvas.height = info.config.displayAspectHeight ?? info.config.codedHeight ?? 1;
 			} else {
 				// Hide the canvas when no broadcast is selected.
