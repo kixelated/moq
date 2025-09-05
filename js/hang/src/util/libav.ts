f31f91d9de6a829747e584209fb09102a31f28d5
Luke Curley <kixelated@gmail.com>
2025-09-05 10:22:46 -0700

    Fix opus boye.

--- js/hang/src/util/libav.ts
+++ js/hang/src/util/libav.ts
@@ -12,15 +12,16 @@ export async function polyfill(): Promise<boolean> {
 		// Load the polyfill and the libav variant we're using.
 		// TODO build with AAC support.
 		// I forked libavjs-webcodecs-polyfill to avoid Typescript errors; there's no changes otherwise.
-		loading = Promise.all([import("@libav.js/variant-opus"), import("@kixelated/libavjs-webcodecs-polyfill")]).then(
-			async ([opus, libav]) => {
-				await libav.load({
-					LibAV: opus,
-					polyfill: true,
-				});
-				return true;
-			},
-		);
+		loading = Promise.all([
+			import("@libav.js/variant-opus-af"),
+			import("@kixelated/libavjs-webcodecs-polyfill"),
+		]).then(async ([opus, libav]) => {
+			await libav.load({
+				LibAV: opus,
+				polyfill: true,
+			});
+			return true;
+		});
 	}
 	return await loading;
 }
