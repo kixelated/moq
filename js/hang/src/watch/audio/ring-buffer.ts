a514afc7b9144b2db64b1f91c53b13349e6f17bf
Luke Curley <kixelated@gmail.com>
2025-09-23 16:08:46 -0400

    Add a video jitter buffer.

--- js/hang/src/watch/audio/ring-buffer.ts
+++ js/hang/src/watch/audio/ring-buffer.ts
@@ -85,7 +85,9 @@ export class AudioRingBuffer {
 
 		// Write the actual samples
 		for (let channel = 0; channel < this.channels; channel++) {
-			const src = data[channel];
+			let src = data[channel];
+			src = src.slice(src.length - samples);
+
 			const dst = this.#buffer[channel];
 			if (src.length !== samples) throw new Error("mismatching number of samples");
 
