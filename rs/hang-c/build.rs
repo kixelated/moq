6f8126d8fb34e5e0a928f8538980c3081dca3165
Luke Curley <kixelated@gmail.com>
2025-11-26 13:49:58 +1100

    Put the .h file in the same build directory.

--- rs/hang-c/build.rs
+++ rs/hang-c/build.rs
@@ -7,9 +7,10 @@ const LIB_NAME: &str = "hang";
 fn main() {
 	let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 	let version = env::var("CARGO_PKG_VERSION").unwrap();
+	let out_dir = output_dir();
 
 	// Generate C header
-	let header = target_dir().join(format!("{}.h", LIB_NAME));
+	let header = out_dir.join(format!("{}.h", LIB_NAME));
 	cbindgen::Builder::new()
 		.with_crate(&crate_dir)
 		.with_language(cbindgen::Language::C)
@@ -19,7 +20,7 @@ fn main() {
 
 	// Generate pkg-config file
 	let pc_in = PathBuf::from(&crate_dir).join(format!("{}.pc.in", LIB_NAME));
-	let pc_out = target_dir().parent().unwrap().join(format!("{}.pc", LIB_NAME));
+	let pc_out = out_dir.join(format!("{}.pc", LIB_NAME));
 	if let Ok(template) = fs::read_to_string(&pc_in) {
 		let content = template
 			.replace("@PREFIX@", "/usr/local")
@@ -28,12 +29,14 @@ fn main() {
 	}
 }
 
-fn target_dir() -> PathBuf {
-	if let Ok(target) = env::var("CARGO_TARGET_DIR") {
-		PathBuf::from(target)
-	} else {
-		PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
-			.join("target")
-			.join("include")
-	}
+fn output_dir() -> PathBuf {
+	// OUT_DIR is like: target/debug/build/hang-c-xxx/out
+	// We want: target/debug/ (where the library is)
+	let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+	out_dir
+		.parent() // build/hang-c-xxx
+		.and_then(|p| p.parent()) // build
+		.and_then(|p| p.parent()) // debug
+		.map(|p| p.to_path_buf())
+		.unwrap_or(out_dir)
 }
