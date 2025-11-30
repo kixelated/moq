15014014f3af668f938ff8da373f39d2fc955af0
James Reetzke <james@reet-ski.net>
2025-11-29 22:18:12 -0500

    if hang instance is available when element is provided, run signal setup immediately

--- js/hang-ui/src/Components/watch/WatchControlsContextProvider.tsx
+++ js/hang-ui/src/Components/watch/WatchControlsContextProvider.tsx
@@ -78,6 +78,9 @@ export default function WatchControlsContextProvider(props: WatchControlsContext
 				const watchInstance = event.detail.instance.peek?.() as HangWatchInstance;
 				onWatchInstanceAvailable(watchInstance);
 			});
+		} else {
+			const hangWatchInstance = hangWatchEl.active.peek?.() as HangWatchInstance;
+			onWatchInstanceAvailable(hangWatchInstance);
 		}
 	});
 
