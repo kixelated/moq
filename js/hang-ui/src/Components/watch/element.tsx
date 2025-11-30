c1879236a6e63e0638b5098248741ec5667b99a4
James Reetzke <james@reet-ski.net>
2025-11-30 00:13:53 -0500

    fix webcomponent name

--- js/hang-ui/src/Components/watch/element.tsx
+++ js/hang-ui/src/Components/watch/element.tsx
@@ -1,34 +1,30 @@
-import type HangWatch from '@kixelated/hang/watch/element';
-import { customElement } from 'solid-element';
-import { createSignal, onMount } from 'solid-js';
-import BufferingIndicator from './BufferingIndicator';
-import styles from './styles.css';
-import WatchControls from './WatchControls';
-import WatchControlsContextProvider from './WatchControlsContextProvider';
+import type HangWatch from "@kixelated/hang/watch/element";
+import { customElement } from "solid-element";
+import { createSignal, onMount } from "solid-js";
+import BufferingIndicator from "./BufferingIndicator";
+import styles from "./styles.css";
+import WatchControls from "./WatchControls";
+import WatchControlsContextProvider from "./WatchControlsContextProvider";
 
-customElement(
-    'hang-publish-ui',
-    {},
-    function PublishControlsWebComponent(_, { element }) {
-        const [hangWatchhEl, setHangWatchEl] = createSignal<HangWatch>();
+customElement("hang-watch-ui", {}, function PublishControlsWebComponent(_, { element }) {
+	const [hangWatchhEl, setHangWatchEl] = createSignal<HangWatch>();
 
-        onMount(() => {
-            const watchEl = element.querySelector('hang-watch');
+	onMount(() => {
+		const watchEl = element.querySelector("hang-watch");
 
-            if (watchEl) {
-                setHangWatchEl(watchEl);
-            }
-        });
+		if (watchEl) {
+			setHangWatchEl(watchEl);
+		}
+	});
 
-        return (
-            <WatchControlsContextProvider hangWatch={hangWatchhEl}>
-                <style>{styles}</style>
-                <div class="watchVideoContainer">
-                    <slot />
-                    <BufferingIndicator />
-                </div>
-                <WatchControls />
-            </WatchControlsContextProvider>
-        );
-    }
-);
+	return (
+		<WatchControlsContextProvider hangWatch={hangWatchhEl}>
+			<style>{styles}</style>
+			<div class="watchVideoContainer">
+				<slot />
+				<BufferingIndicator />
+			</div>
+			<WatchControls />
+		</WatchControlsContextProvider>
+	);
+});
