af16caeca5463e0e9092c19cb3ce73bf8ea18d37
Luke Curley <kixelated@gmail.com>
2025-12-09 22:57:30 +1100

    Rename JS folders to match the package name (#751)

--- js/lite/src/index.ts
+++ js/lite/src/index.ts
@@ -2,7 +2,5 @@ export * from "./announced.ts";
 export * from "./broadcast.ts";
 export * as Connection from "./connection/index.ts";
 export * from "./group.ts";
-export * as Ietf from "./ietf/index.ts";
-export * as Lite from "./lite/index.ts";
 export * as Path from "./path.ts";
 export * from "./track.ts";
